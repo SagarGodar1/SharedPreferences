a6d2fe9d074cbc328c9bce62cf1b727ee8009b61
Softwarica College Lab <softwarica@softwarica.com>
2019-04-28 11:28:02 +0545

    Initial commit

---
+++ app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.sharedpreferences;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText etUsername,etPassword;
+    private Button btnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+        btnRegister = findViewById(R.id.btnRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+SignUp();
+            }
+
+        });
+    }
+
+
+    private void SignUp(){
+        SharedPreferences sharedPreferences = getSharedPreferences("User",MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString("username",etUsername.getText().toString());
+        editor.putString("password",etPassword.getText().toString());
+        editor.commit();
+        Toast.makeText(this, "Successfully Registered", Toast.LENGTH_SHORT).show();
+    }
+    private void checkUser(){
+        SharedPreferences sharedPreferences = getSharedPreferences("User", MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "");
+        String password = sharedPreferences.getString("password", "");
+
+        if (username.equals(etUsername.getText().toString()) ||
+        password.equals(etPassword.getText().toString())) {
+            Toast.makeText(this, "Successfull", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+}
